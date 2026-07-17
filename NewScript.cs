4df5e08bc1278fe98e4d332a5175106562f17844
Maromitaz <boltaalexdan@gmail.com>
2026-07-17 21:03:55 +0300

    Learned Collisions

--- NewScript.cs
+++ NewScript.cs
@@ -1,45 +1,24 @@
 using Godot;
-using System;
-using System.Numerics;
 
-public partial class NewScript : RigidBody2D
+public partial class NewScript : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-        //this.Position = new Vector2(0.f, 0.f);
+    [Export]
+    float speed { get; set; } = 400.0f;
+
+    public void GetInput()
+    {
+        var inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        Velocity = inputDirection * speed;
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-    float speed = 100.0f;
-    //public override void _Process(double delta)
-    //{
-    //    if (Input.IsActionPressed(action: "ui_right"))
-    //    {
-    //        this.Position = this.Position + new Godot.Vector2((float)delta * speed, .0f);
-    //    }
-    //    if (Input.IsActionPressed(action: "ui_left"))
-    //    {
-    //        this.Position = this.Position - new Godot.Vector2((float)delta * speed, .0f);
-    //    }
-    //    if (Input.IsActionPressed(action: "ui_up"))
-    //    {
-    //        this.Position = this.Position - new Godot.Vector2(.0f, (float)delta * speed);
-    //    }
-    //    if (Input.IsActionPressed(action: "ui_down"))
-    //    {
-    //        this.Position = this.Position + new Godot.Vector2(.0f, (float)delta * speed);
-    //    }
-    //}
     public override void _PhysicsProcess(double delta)
     {
-        //var velocity = Velocity;
-        var collision = MoveAndCollide(LinearVelocity * (float)delta);
+        this.GetInput();
+        var collision = MoveAndCollide(Velocity * (float)delta);
         if (collision != null)
         {
-            var collisionpoint = collision.GetPosition();
-            GD.Print(collision);
+            Velocity = Velocity.Slide(collision.GetNormal());
         }
-        //base._PhysicsProcess(delta);
+        //MoveAndSlide();
     }
 }
