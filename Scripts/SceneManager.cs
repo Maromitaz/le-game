5c0fe91928f3de389b3861a17c276533f9ec1836
Maromitaz <boltaalexdan@gmail.com>
2026-07-18 22:54:22 +0300

    Changes

--- Scripts/SceneManager.cs
+++ Scripts/SceneManager.cs
@@ -8,7 +8,7 @@ public partial class SceneManager : Node
 
 	PackedScene[] Scenes =
     {
-        GD.Load<PackedScene>("res::/Scenes/map_1.tscn"),
+        GD.Load<PackedScene>("res://Scenes/node_2d.tscn"),
     };
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -20,5 +20,6 @@ public partial class SceneManager : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	}
+		//this.GetChild(0).QueueFree();
+    }
 }
