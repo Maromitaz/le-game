1125061b0a18c4f2e43528726a1836c24f9e0326
Maromitaz <boltaalexdan@gmail.com>
2026-07-19 00:21:24 +0300

    Test

--- Scripts/SceneManager.cs
+++ Scripts/SceneManager.cs
@@ -20,6 +20,7 @@ public partial class SceneManager : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GD.Print(this.GetChild(0));
 		//this.GetChild(0).QueueFree();
     }
 }
