cc5d6974809eef87eb177132bf5ed440eae7a886
Maromitaz <boltaalexdan@gmail.com>
2026-07-23 22:18:43 +0300

    Made options menu for the pause menu. IDK what can work though. We might need to use forward rendering for desktops and see afterwards what we can do about other platforms. Might not make web builds, maybe only for the demo.

--- Scripts/PauseMenu.cs
+++ Scripts/PauseMenu.cs
@@ -3,17 +3,20 @@ using System;
 
 public partial class PauseMenu : Control
 {
-	public void Pause()
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void Pause()
 	{
 		GetTree().Paused = true;
-		this.GetChild<AnimationPlayer>(1).Play("pause");
+        this.Visible = true;
+        this.GetChild<AnimationPlayer>(-1).Play("pause");
 	}
 
 	public void Resume()
 	{
 		GetTree().Paused = false;
-        this.GetChild<AnimationPlayer>(1).PlayBackwards("pause");
-	}
+        this.GetChild<AnimationPlayer>(-1).PlayBackwards("pause");
+        this.Visible = false;
+    }
 
 	public void IsEscPressed()
 	{
@@ -29,7 +32,15 @@ public partial class PauseMenu : Control
 
     public void _on_resume_game_pressed()
     {
-		Resume();
+        Resume();
+    }
+    public void _on_options_pressed()
+    {
+        Control pauseContainer = this.GetNode<Control>("Pause Container");
+        pauseContainer.Visible = false;
+
+        Control optionsContainer = this.GetNode<Control>("Options Container");
+        optionsContainer.Visible = true;
     }
 
     public void _on_main_menu_pressed()
@@ -37,13 +48,42 @@ public partial class PauseMenu : Control
         Globals Globals = GetNode<Globals>("/root/Globals");
         Globals.GoToScene(Globals.Scenes.MainMenu);
         GetTree().Paused = false;
-        //SceneManager sceneManager = (SceneManager)this.FindParent("SceneManager");
-        //sceneManager.EmitSignal("ChangeScene", "MainMenu");
     }
 
     public void _on_quit_game_pressed()
     {
 		GetTree().Quit();
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void _on_back_pressed()
+    {
+        Control pauseContainer = this.GetNode<Control>("Pause Container");
+        pauseContainer.Visible = true;
+
+        Control optionsContainer = this.GetNode<Control>("Options Container");
+        optionsContainer.Visible = false;
+    }
+
+    public void _on_full_screen_pressed()
+    {
+        if (DisplayServer.WindowGetSize().X == 1152)
+        {
+            DisplayServer.WindowSetSize(new Vector2I(800, 640));
+        }
+        if (DisplayServer.WindowGetSize().X == 800)
+        {
+            DisplayServer.WindowSetSize(new Vector2I(1152, 648));
+        }
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+    public override void _Ready()
+    {
+        Control optionsContainer = this.GetNode<Control>("Options Container");
+        optionsContainer.Visible = false;
+        this.Visible = false;
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
